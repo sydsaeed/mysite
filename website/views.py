b3d49790eb5a86a62c00b346c85d3d221f2eae71
saeed mohaghegh <sydsaeed.gerash@gmail.com>
2026-07-25 08:31:13 +0330

    add statics and templates

--- website/views.py
+++ website/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(requests):
-    return render(requests, 'index.html')
+    return render(requests, 'website/index.html')
 
 def about_view(requests):
-    return render(requests, 'about.html')
+    return render(requests, 'website/about.html')
 
 def contact_view(requests):
-    return render(requests, 'contact.html')
+    return render(requests, 'website/contact.html')
 
