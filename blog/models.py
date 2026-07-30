20cfee6df57e4c8dd648cdc293565b119ba213bf
saeed mohaghegh <sydsaeed.gerash@gmail.com>
2026-07-30 12:01:44 +0330

    add 2 table

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 class Post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=200)
     content = models.TextField()
+    
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
