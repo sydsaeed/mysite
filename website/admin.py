2186dc346d60133d1727f54b925009b56a007865
saeed mohaghegh <sydsaeed.gerash@gmail.com>
2026-07-30 17:38:20 +0330

    change admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from website.models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','email','created_date')
+    list_filter = ('email',)
+    search_fields = ('name','massage')
+    date_hierarchy = 'created_date'
+
+admin.site.register(Contact, ContactAdmin)
--- website/models.py
+++ website/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     massage = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
